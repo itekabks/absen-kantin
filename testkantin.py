da4628487a8438495cb4759290b3c2ca083bc157
itekabks <iteka.bks@gmail.com>
2026-09-07 08:32:38 +0700

    Update testkantin.py

--- testkantin.py
+++ testkantin.py
@@ -94,6 +94,10 @@ st.markdown("<h1 style='text-align: center; color: #1e293b; text-shadow: 1px 1px
 # Petunjuk Penulisan NIK
 st.markdown("<p style='text-align: center; color: #475569; font-weight: 600; font-size: 1rem; text-shadow: 1px 1px 1px rgba(255,255,255,0.8); margin-bottom: 25px;'>Untuk penulisan NIK menggunakan 000NIK</p>", unsafe_allow_html=True)
 
+# Contoh Penulisan NIK
+st.markdown("<p style='text-align: center; color: #475569; font-weight: 600; font-size: 1rem; text-shadow: 1px 1px 1px rgba(255,255,255,0.8); margin-bottom: 25px;'>Contoh 00003950</p>", unsafe_allow_html=True)
+
+
 # URL & Entry Google Form versi TEST
 FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLScnTi-b9vCrBSRMr-G7k3_4buevp02nJ9J6ybkatj5SGCKKfw/formResponse"
 ENTRY_NIK = "entry.952185819"
