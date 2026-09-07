670d772a584e036fc15d512f419d1f81791ce515
itekabks <iteka.bks@gmail.com>
2026-09-07 07:31:27 +0700

    Update testkantin.py

--- testkantin.py
+++ testkantin.py
@@ -74,7 +74,7 @@ st.markdown(custom_css, unsafe_allow_html=True)
 # Judul Utama
 st.markdown("<h1 style='text-align: center; color: #1e293b; text-shadow: 1px 1px 2px rgba(255,255,255,0.8);'>📌 Absensi Kantin Eka Bekasi</h1>", unsafe_allow_html=True)
 
-FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLScnTi-b9vCrBSRMr-G7k3_4buevp02nJ9J6ybkatj5SGCKKfw/viewform?usp=dialog"
+FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLScnTi-b9vCrBSRMr-G7k3_4buevp02nJ9J6ybkatj5SGCKKfw/viewform"
 ENTRY_NIK = "entry.952185819"
 
 # Form Input Absen
